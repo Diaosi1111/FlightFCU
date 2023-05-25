d8440c67433ac66e987db02fabaecf6f9d39760b
Alex Newton <sharenfan222@gmail.com>
2023-05-26 02:07:47 +0800

    air usb

---
+++ Core/Inc/platform_config.h
@@ -0,0 +1,86 @@
+/**
+ ******************************************************************************
+ * @file    platform_config.h
+ * @author  MCD Application Team
+ * @version V4.0.0
+ * @date    21-January-2013
+ * @brief   Evaluation board specific configuration file.
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; COPYRIGHT 2013 STMicroelectronics</center></h2>
+ *
+ * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *        http://www.st.com/software_license_agreement_liberty_v2
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************
+ */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __PLATFORM_CONFIG_H
+#define __PLATFORM_CONFIG_H
+
+/* Includes ------------------------------------------------------------------*/
+
+#include "stm32f1xx.h"
+
+#define BITBAND(addr, bitnum)  (((addr)&0xF0000000) + 0x2000000 + (((addr)&0xFFFFF) << 5) + ((bitnum) << 2))
+#define MEM_ADDR(addr)         *((volatile unsigned long *)(addr))
+#define BIT_ADDR(addr, bitnum) MEM_ADDR(BITBAND(addr, bitnum))
+
+#define GPIOA_ODR_Addr         (GPIOA_BASE + 12) // 0x4001080C
+#define GPIOB_ODR_Addr         (GPIOB_BASE + 12) // 0x40010C0C
+#define GPIOC_ODR_Addr         (GPIOC_BASE + 12) // 0x4001100C
+#define GPIOD_ODR_Addr         (GPIOD_BASE + 12) // 0x4001140C
+#define GPIOE_ODR_Addr         (GPIOE_BASE + 12) // 0x4001180C
+#define GPIOF_ODR_Addr         (GPIOF_BASE + 12) // 0x40011A0C
+#define GPIOG_ODR_Addr         (GPIOG_BASE + 12) // 0x40011E0C
+
+#define GPIOA_IDR_Addr         (GPIOA_BASE + 8) // 0x40010808
+#define GPIOB_IDR_Addr         (GPIOB_BASE + 8) // 0x40010C08
+#define GPIOC_IDR_Addr         (GPIOC_BASE + 8) // 0x40011008
+#define GPIOD_IDR_Addr         (GPIOD_BASE + 8) // 0x40011408
+#define GPIOE_IDR_Addr         (GPIOE_BASE + 8) // 0x40011808
+#define GPIOF_IDR_Addr         (GPIOF_BASE + 8) // 0x40011A08
+#define GPIOG_IDR_Addr         (GPIOG_BASE + 8) // 0x40011E08
+
+#define PAout(n)               BIT_ADDR(GPIOA_ODR_Addr, n) // ���
+#define PAin(n)                BIT_ADDR(GPIOA_IDR_Addr, n) // ����
+
+#define PBout(n)               BIT_ADDR(GPIOB_ODR_Addr, n) // ���
+#define PBin(n)                BIT_ADDR(GPIOB_IDR_Addr, n) // ����
+
+#define PCout(n)               BIT_ADDR(GPIOC_ODR_Addr, n) // ���
+#define PCin(n)                BIT_ADDR(GPIOC_IDR_Addr, n) // ����
+
+#define PDout(n)               BIT_ADDR(GPIOD_ODR_Addr, n) // ���
+#define PDin(n)                BIT_ADDR(GPIOD_IDR_Addr, n) // ����
+
+#define PEout(n)               BIT_ADDR(GPIOE_ODR_Addr, n) // ���
+#define PEin(n)                BIT_ADDR(GPIOE_IDR_Addr, n) // ����
+
+#define PFout(n)               BIT_ADDR(GPIOF_ODR_Addr, n) // ���
+#define PFin(n)                BIT_ADDR(GPIOF_IDR_Addr, n) // ����
+
+#define PGout(n)               BIT_ADDR(GPIOG_ODR_Addr, n) // ���
+#define PGin(n)                BIT_ADDR(GPIOG_IDR_Addr, n) // ����
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Uncomment the line corresponding to the STMicroelectronics evaluation board
+   used to run the example */
+
+#define USE_STM3210E_EVAL // STM32F10X使用的版本
+
+#endif /* __PLATFORM_CONFIG_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -27,7 +27,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-// #include "usbd_customhid.h"
+#include "usbd_customhid.h"
 #include "stdio.h"
 #include "bsp_key.h"
 #include "u8g2.h"
@@ -103,12 +103,12 @@ uint8_t __io_putchar(int ch)
     return ch;
 }
 
-// extern USBD_HandleTypeDef hUsbDeviceFS;
-//
-// static int8_t USBD_CUSTOM_HID_SendReport_FS(uint8_t *report, uint16_t len)
-//{
-//     return USBD_CUSTOM_HID_SendReport(&hUsbDeviceFS, report, len);
-// }
+extern USBD_HandleTypeDef hUsbDeviceFS;
+
+static int8_t USBD_CUSTOM_HID_SendReport_FS(uint8_t *report, uint16_t len)
+{
+    return USBD_CUSTOM_HID_SendReport(&hUsbDeviceFS, report, len);
+}
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -251,40 +251,42 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
+    /* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+    /* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+    /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
 
-  /* USER CODE BEGIN Init */
+    /* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+    /* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+    /* Configure the system clock */
+    SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+    /* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+    /* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_DMA_Init();
-  MX_I2C1_Init();
-  MX_I2C2_Init();
-  MX_USART1_UART_Init();
-  MX_TIM3_Init();
-  MX_USB_DEVICE_Init();
-  /* USER CODE BEGIN 2 */
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_DMA_Init();
+    MX_I2C1_Init();
+    MX_I2C2_Init();
+    MX_USART1_UART_Init();
+    MX_TIM3_Init();
+#define DP_PUUP *((__IO unsigned *)((0x40005C00L) + 0x54))
+    DP_PUUP = 1;
+    MX_USB_DEVICE_Init();
+    /* USER CODE BEGIN 2 */
     /*------------ 初始化 ---------------*/
     bsp_key_init();
 
@@ -293,19 +295,19 @@ int main(void)
     u8g2_init_hi2c2(&screen_left);
     u8g2_init_hi2c1(&screen_right);
 
-    fcu_state.trk_fpa_mode = 1;
-    fcu_state.spd_dot      = 1;
-    fcu_state.hdg_dot      = 1;
-  /* USER CODE END 2 */
+    //    fcu_state.trk_fpa_mode = 1;
+    //    fcu_state.spd_dot      = 1;
+    //    fcu_state.hdg_dot      = 1;
+    /* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
     while (1) {
-    /* USER CODE END WHILE */
+        /* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
+        /* USER CODE BEGIN 3 */
         //        printf("2s\n");
-        HAL_GPIO_TogglePin(ATHR_LED_GPIO_Port, ATHR_LED_Pin);
+        //        HAL_GPIO_TogglePin(ATHR_LED_GPIO_Port, ATHR_LED_Pin);
         /* ----------------------- 按键扫描 ------------------------- */
         //        matrix_key_scan();
         KEY_ENUM key = bsp_key_dequeue();
@@ -468,8 +470,9 @@ int main(void)
 
         /* ----------------------- LED刷新 ------------------------- */
         //        panel_led_update(&fcu_state);
-//        fcu_state.trk_fpa_mode=1-fcu_state.trk_fpa_mode;
+        //        fcu_state.trk_fpa_mode=1-fcu_state.trk_fpa_mode;
         /* ----------------------- 显示刷新 ------------------------- */
+        //        if (hUsbDeviceFS.dev_state == USBD_STATE_CONFIGURED) { // USB 已连接
         /* Left Screen */
         u8g2_ClearBuffer(&screen_left);
 
@@ -546,54 +549,53 @@ int main(void)
         u8g2_DrawHLine(&screen_right, VS_H_POS + 10, 3, 10);
 
         u8g2_SendBuffer(&screen_right);
+        //        }
+        //        else{
+        //        }
     }
-  /* USER CODE END 3 */
+    /* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+    RCC_OscInitTypeDef RCC_OscInitStruct   = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct   = {0};
+    RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    /** Initializes the RCC Oscillators according to the specified parameters
+     * in the RCC_OscInitTypeDef structure.
+     */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState       = RCC_HSE_ON;
+    RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+    RCC_OscInitStruct.HSIState       = RCC_HSI_ON;
+    RCC_OscInitStruct.PLL.PLLState   = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource  = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLMUL     = RCC_PLL_MUL6;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+    /** Initializes the CPU, AHB and APB buses clocks
+     */
+    RCC_ClkInitStruct.ClockType      = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
-  PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL_DIV1_5;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+        Error_Handler();
+    }
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
+    PeriphClkInit.UsbClockSelection    = RCC_USBCLKSOURCE_PLL_DIV1_5;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
+        Error_Handler();
+    }
 }
 
 /* USER CODE BEGIN 4 */
@@ -601,32 +603,32 @@ void SystemClock_Config(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
+    /* USER CODE BEGIN Error_Handler_Debug */
     /* User can add his own implementation to report the HAL error return state */
     __disable_irq();
     while (1) {
     }
-  /* USER CODE END Error_Handler_Debug */
+    /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
+    /* USER CODE BEGIN 6 */
     /* User can add his own implementation to report the file name and line number,
        ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+    /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
